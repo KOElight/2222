c7b9d0dc0332674e5b158e47c0ce7541a8fd3063
KOElight <792212278@qq.com>
2019-08-15 11:18:56 +0800

    111

---
+++ 2222/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2222
+//
+//  Created by koe on 2019/8/14.
+//  Copyright © 2019 koe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 2222Tests/_222Tests.m
@@ -0,0 +1,37 @@
+//
+//  _222Tests.m
+//  2222Tests
+//
+//  Created by koe on 2019/8/14.
+//  Copyright © 2019 koe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _222Tests : XCTestCase
+
+@end
+
+@implementation _222Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
